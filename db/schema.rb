47030f3d551a5c365f4fa785474c92e74f96bd3d
ernestognw <ernestognw@gmail.com>
2018-12-23 16:32:57 -0600

    Added dynamic post views, ckeditor and paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -24,10 +24,17 @@ ActiveRecord::Schema.define(version: 2018_12_23_210101) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "task"
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "data_fingerprint"
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "users", force: :cascade do |t|
